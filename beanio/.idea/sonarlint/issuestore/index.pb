2bcb8f971e180fdab4ad3941615fd5a56fcb69d6
João Carlos Porto dos Reis <joao.reis@cabal.com.br>
2020-02-19 13:03:45 -0300

    Estudos iniciais BeanIO

---
+++ beanio/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+O
+src/main/java/model/Pessoa.java,4\0\40bf33e611fe4549a2f3eebd7c98250d53be6b9a
+T
+$src/main/java/BeanReaderExample.java,2\c\2c0710c58bd0f52bf29bf8d4499d5e8dc957d575
+N
+src/main/resources/mapping.xml,0\4\0429bde23c6c83b0fac943ff7bd2e007c2f31665
+T
+$src/main/java/BeanWriterExample.java,4\9\49bc99a27942fbdacfe7b76d6b1319daf418b15a
---
+++ beanio/src/main/java/model/Pessoa.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.util.Date;
+
+public class Pessoa {
+
+    String firstName;
+    String lastName;
+    String title;
+    String salary;
+    Date hireDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Pessoa() {
+    }
+}
